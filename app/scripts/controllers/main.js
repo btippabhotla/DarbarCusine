6370e53c58198da1cb2e1381612d8ef2f60b1a75
Bharadwaj Tippabhotla <btippabhotla@localhost.localdomain>
2015-10-04 13:45:05 -0700

    Adding file structure to hotel site

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name darbarCusineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the darbarCusineApp
+ */
+angular.module('darbarCusineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
