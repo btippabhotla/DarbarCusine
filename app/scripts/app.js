a3aa52e24e85c41e6c7298b6d14a392ca3eca519
btippabhotla <bharadwajt10@gmail.com>
2015-10-04 23:27:55 -0700

    updates to index

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/menu', {
+        templateUrl: 'views/menu.html',
+        controller: 'MenuCtrl',
+        controllerAs: 'menu'
+      })
       .otherwise({
         redirectTo: '/'
       });
