852f551fd646d42fc1c6f70464b3d1385c3cb85f
btippabhotla <bharadwajt10@gmail.com>
2016-05-01 01:12:27 -0700

    updates to the site

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -1,23 +1,14 @@
-'use strict';
+/*
+  'use strict';
 
-/**
- * @ngdoc function
- * @name darbarCusineApp.controller:AboutCtrl
- * @description
- * # AboutCtrl
- * Controller of the darbarCusineApp
- */
-angular.module('darbarCusineApp')
-  .controller('MenuCtrl', $scope, $http,  function ($scope, $http) {
-    
-    /*$http.get("JsonData/menu.json").success(function (response) {
-            $scope.names = response.data;
-    console.log($scope.names);
-}).error(function (error){
-    console.log("you got an error");
-});*/
+angular.module('darbarCusineApp', [])
+  .controller('MenuCtrl', ['$scope', '$http', function ($scope, $http) {
 
-  });
+    $http.get("JsonData/Appetizers.json").success(function (response) {
+           $scope.names = response.data;
+           console.log($scope.names);
+});
 
+  }]); 
 
-  
+*/
